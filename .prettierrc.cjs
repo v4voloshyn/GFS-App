f43ff5505c9b29f51076aed0a37327c0004aba05
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-03-14 22:32:22 +0200

    Add ESLint and Prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  printWidth: 80,
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  endOfLine: "auto",
+};
