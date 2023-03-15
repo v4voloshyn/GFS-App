166774c168990588001e37b1580520cb46b065d0
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-03-15 16:42:29 +0200

    Manage ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,5 +21,7 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'react/function-component-definition': 0,
+    'import/prefer-default-export': 0,
   },
 };
