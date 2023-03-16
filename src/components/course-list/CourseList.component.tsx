49f8119a027436ded6151814ee0c7b3834bf687b
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-03-16 23:53:02 +0200

    Feat: CourseList component

--- src/components/course-list/CourseList.component.tsx
+++ src/components/course-list/CourseList.component.tsx
@@ -1,15 +1,22 @@
-import { FC, useState, useEffect } from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import { FC, useEffect } from 'react';
 import { fetchCourses } from '../../helpers';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { ICourseItem } from '../../types/types';
 import { CourseItem } from '../course-item/CourseItem.component';
 
+import './CourseList.scss';
+
 export const CourseList: FC = () => {
-  const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useLocalStorage<ICourseItem[]>('courses', []);
 
   useEffect(() => {
+    if (courses.length) return;
+
     fetchCourses()
-      .then((data) => console.log(data))
+      .then((data) => setCourses(data))
       .catch((err) => console.log(err));
-  }, []);
+  }, [courses, setCourses]);
 
   return (
     <div className="course-list">
@@ -17,7 +24,9 @@ export const CourseList: FC = () => {
         Course List
       </h1>
       <div className="courses">
-        <CourseItem />
+        {courses.map((course) => (
+          <CourseItem key={course.id} {...course} />
+        ))}
       </div>
     </div>
   );
