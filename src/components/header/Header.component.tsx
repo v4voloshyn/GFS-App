9b6d7d92351f185bcb8c6b95f2801bd7c3814baa
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-03-16 23:51:26 +0200

    Add logo and fake user avatar to Header

--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -1,19 +1,22 @@
 import { FC } from 'react';
 import { FaUserGraduate } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import './Header.scss';
 
 export const Header: FC = () => {
   return (
     <div className="header">
-      <div className="logo">
-        <img
-          src="/logo.png"
-          height="100%"
-          alt="GFS Logo"
-          className="logo__img"
-        />
-        <span>GFS APP</span>
-      </div>
+      <Link to="/">
+        <div className="logo">
+          <img
+            src="/logo.png"
+            height="100%"
+            alt="GFS Logo"
+            className="logo__img"
+          />
+          <span>GFS APP</span>
+        </div>
+      </Link>
       <div className="user">
         <FaUserGraduate className="user__avatar" />
         <span className="user__name">User Name</span>
