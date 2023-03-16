bd42d6573d3c4a474d2d17f37cb3a7e11710af32
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-03-16 23:38:55 +0200

    Add types

---
+++ src/types/types.ts
@@ -0,0 +1,26 @@
+export interface ICourseVideoPreview {
+  link: string;
+  duration: number;
+  previewImageLink: string;
+}
+
+export interface IMeta {
+  slug: string;
+  skills: string[];
+  courseVideoPreview: ICourseVideoPreview;
+}
+
+export interface ICourseItem {
+  id: string;
+  title: string;
+  tags: string[];
+  launchDate: Date;
+  status: string;
+  description: string;
+  duration: number;
+  lessonsCount: number;
+  containsLockedLessons: boolean;
+  previewImageLink: string;
+  rating: number;
+  meta: IMeta;
+}
