736611a11468955ea20645f52b76f85a31cd9708
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-03-16 23:37:00 +0200

    Disable eslint no-console rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,5 +23,6 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'react/function-component-definition': 0,
     'import/prefer-default-export': 0,
+    'no-console': 0, // Remove before deploy
   },
 };
