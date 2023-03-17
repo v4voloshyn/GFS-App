96d5706dd18c266106915495ee4af4ec766ceccf
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-03-17 22:30:49 +0200

    Feat: add Spinner on App loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
+import { Spinner } from './components/spinner/Spinner.component';
 import { router } from './routes/routes';
 
 export const App = () => {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} fallbackElement={<Spinner />} />;
 };
---
+++ src/components/spinner/Spinner.component.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import './Spinner.scss';
+
+export const Spinner: FC = () => {
+  return <div className="spinner" />;
+};
