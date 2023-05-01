18a6420f08f17e8cd6405c8359bfe078005645b5
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-05-01 12:33:20 +0300

    feat: add error catching in getRequest api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,17 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { AxiosResponse } from 'axios';
+import { AxiosError, AxiosResponse } from 'axios';
 import { instance } from './instance';
+
+type CustomErrorResponse = {
+  message: string;
+  statusCode: number;
+};
 
 export const getRequest = async <T>(
   endpoint: string,
   params: Record<string, any> = {}
 ): Promise<T> => {
-  const request: AxiosResponse<T> = await instance.get(endpoint, {
-    params,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  try {
+    const request: AxiosResponse<T> = await instance.get(endpoint, {
+      params,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-  return request.data;
+    return request.data;
+  } catch (requestError) {
+    const axiosError = requestError as AxiosError<CustomErrorResponse>;
+
+    if (axiosError) {
+      const errorObj = {
+        statusCode: axiosError.response?.data.statusCode,
+        message: axiosError.response?.data.message,
+      };
+
+      throw new Error(`${errorObj.statusCode} - ${errorObj.message}`);
+    } else {
+      throw requestError;
+    }
+  }
 };
