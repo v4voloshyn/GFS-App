dc24f708b2d51378fffe5a299d611c18d7966989
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-05-06 21:25:40 +0300

    Feat: add Spinner test

--- src/components/common/UI/spinner/Spinner.component.tsx
+++ src/components/common/UI/spinner/Spinner.component.tsx
@@ -2,19 +2,23 @@ import { FC } from 'react';
 
 import './Spinner.scss';
 
-interface SpinnerProps {
+export interface SpinnerProps {
   variant?: 'small' | 'fullscreen';
 }
 
 export const Spinner: FC<SpinnerProps> = ({ variant = 'small' }) => {
   if (variant === 'fullscreen') {
-    return <div className="spinner" />;
+    return <div className="spinner" role="status" aria-label="loading" />;
   }
 
   return (
     <div className="spinner-wrapper">
       <span>Loading...</span>
-      <div className="spinner spinner-small" />
+      <div
+        className="spinner spinner-small"
+        role="status"
+        aria-label="loading"
+      />
     </div>
   );
 };
