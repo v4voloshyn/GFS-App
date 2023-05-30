b47fca5194cebe22d3de750952b5f7eeb34042d7
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-05-30 09:19:39 +0300

    feat: connect to external api server

--- src/shared/api/constants.ts
+++ src/shared/api/constants.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const EXTERNAL_SERVER_LINK =
+  'https://anatoly-skuba-8t2gf5cqm19194vz.socketxp.com/api/v1';
+export const BASE_URL =
+  EXTERNAL_SERVER_LINK || import.meta.env.VITE_API_BASE_URL;
