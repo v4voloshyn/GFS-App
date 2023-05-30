a443cc74572711f9496d5a79794fe92e22da222e
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-05-30 14:13:47 +0300

    fix: base url

--- src/shared/api/constants.ts
+++ src/shared/api/constants.ts
@@ -1,4 +1,4 @@
 const EXTERNAL_SERVER_LINK =
   'https://anatoly-skuba-8t2gf5cqm19194vz.socketxp.com/api/v1';
 export const BASE_URL =
-  EXTERNAL_SERVER_LINK || import.meta.env.VITE_API_BASE_URL;
+  import.meta.env.VITE_API_BASE_URL || EXTERNAL_SERVER_LINK;
